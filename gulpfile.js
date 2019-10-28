95163c93f4e3b21a7eeccb376da2df39c9c8fdab
Ian Ferguson <ian.ferguson@ontario.ca>
2019-10-28 11:14:08 -0400

    * Reworks SASS functions, making things more DRY by using a single handler * Adds the ability for CSS debugging. When enabled, CSS output will include a comment above, specifying which SCSS line generated the output * Adds a rule to clean the dist directory (gulp clean) * Changes the default rule's gulp.series to gulp.parallel as these tasks call all be run async

--- gulpfile.js
+++ gulpfile.js
@@ -4,52 +4,79 @@ const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const minify = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const del = require('del');
+const gulpif = require('gulp-if');
 
+const distDir = 'dist';
 const srcDir = 'src/**';
-const styleMain = 'src/sass/ods-theme.scss';
 const styleDir = 'src/sass/**';
 
-const sassOptions = {
-  outputStyle: 'expanded',
-  includePaths: [
-    '..',
-    '../..',
-    '../../..'
-  ]
-}
+/**
+ * @param {{
+ *   compress:boolean,
+ *   sourcemaps:boolean,
+ *   callback:function,
+ *   [debug]:boolean
+ * }} opts Configuration options
+ */
+const processSass = (opts) => {
+
+  const sassOptions = {
+    outputStyle: 'expanded',
+  };
+
+  if (opts.debug) {
+    sassOptions.sourceComments = true;
+  }
+
+  gulp.src('src/sass/ods-theme.scss')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(concat(gulpif(opts.compress, 'ods-theme.min.css', 'ods-theme.css')))
+    .pipe(gulpif(opts.sourcemaps, sourcemaps.init()))
+    .pipe(gulpif(opts.compress, minify()))
+    .pipe(gulpif(opts.sourcemaps, sourcemaps.write('.')))
+    .pipe(gulp.dest(`${distDir}/css/`));
+
+  if (opts.callback) {
+    opts.callback();
+  }
+};
 
 gulp.task('sass', (done) => {
-  gulp.src(styleMain)
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(concat('ods-theme.css'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/css/'));
-  done();
+  processSass({
+    compress: false,
+    sourcemaps: true,
+    debug: false,
+    callback: done
+  });
 });
 
 gulp.task('sass-minify', (done) => {
-  gulp.src(styleMain)
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(concat('ods-theme.min.css'))
-    .pipe(minify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/css/'));
-  done();
+  processSass({
+    compress: true,
+    sourcemaps: true,
+    callback: done,
+  });
 });
 
+gulp.task('sass:both', gulp.parallel('sass', 'sass-minify'));
+
 gulp.task('assets-move', (done) => {
-  gulp.src([srcDir, `!${styleDir}`], { base: './src' })
-    .pipe(gulp.dest('dist/'));
+  gulp.src([srcDir, styleDir], {base: './src'})
+    .pipe(gulp.dest(distDir));
   done();
 });
 
 gulp.task('watch', (done) => {
-  gulp.watch(styleDir, { ignoreInitial: false }, gulp.series('sass'));
+  gulp.watch(styleDir, {ignoreInitial: false}, gulp.parallel('sass:both'));
   done();
 });
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('clean', (done) => {
+  del(distDir);
+  done();
+});
 
-gulp.task('deploy', gulp.series('assets-move', 'sass', 'sass-minify'));
+gulp.task('deploy', gulp.series('clean', gulp.parallel('assets-move', 'sass:both')));
+
+gulp.task('default', gulp.series('watch'));
